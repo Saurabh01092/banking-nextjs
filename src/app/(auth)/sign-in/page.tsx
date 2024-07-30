0cc24586a57f3920b22e25c48d2db0d767e601e7
Saurabh01092 <saurabh01.1815@gmail.com>
2024-07-30 20:46:37 +0530

    Implement auth, appwrite, sentry

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import AuthForm from "@/src/components/AuthForm"
+
 
 const SignIn = () => {
   return (
-    <div>
-      SigIn
-    </div>
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-in" />
+    </section>
   )
 }
 
